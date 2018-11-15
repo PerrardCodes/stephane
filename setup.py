a0fe766af42bedf307cc71a4c691dd951101c383
sperrard <44366953+sperrard@users.noreply.github.com>
2018-11-15 09:46:23 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='stephane_codes',
+    version='0.1',
+    description='Misc utilities (Stephane Perrard)',
+#      url='needs a URL',
+    author='Stephane Perrard',
+    author_email='stephane.perrard@phys.ens.fr',
+    license='MIT',
+    packages=['stephane,piv3d'],
+    zip_safe=False,
+#      package_data={'tangle': ['cl_src/*.cl']})
+)
